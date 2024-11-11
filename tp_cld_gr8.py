c080c6a9e786450dfecc07e6f44a6c3dce34eab6
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-11-10 23:16:51 -0300

    Conteplamos los paises continente y paises dobles

--- tp_cld_gr8.py
+++ tp_cld_gr8.py
@@ -41,30 +41,103 @@ for index, _ in GSAF.iterrows():
 #%%
 #Vemos como 500 de invalids azules represante cerca del 10% de los datos
 #Decidimos ver que proporcion Hay de azueles y si hay mucho le asignamos QUESTIONABLE
+porcentaje_inconsistencias=len(filas_inconsistentes)/len(GSAF)*100
 
 invalid_azul=[x[0] for x in filas_inconsistentes if x[1]=="INVALID" and x[2]==16764057]
 porcentaje_invalid_azul=len(invalid_azul)/len(filas_inconsistentes)*100
 #Hay un 99,27 de invalid azules, decidimos poner como cuestionable toda la lista
 #Ya que hay menos de un 1% de error
+#%% Veamos que hacer con los paises dobles y continentes
+paises_dobles=sql^"""SELECT Country FROM GSAF WHERE Country LIKE '%/%'"""
+continentes=sql^"""SELECT Country FROM GSAF 
+                WHERE COUNTRY='AFRICA' OR COUNTRY='ASIA?'"""
+#como son 11 registros de 7000 podemos eliminarlos
+paises_a_eliminar=[]
+for index, _ in paises_dobles.iterrows():
+    paises_a_eliminar.append(paises_dobles.iloc[index,0])
+for index, _ in continentes.iterrows():
+    paises_a_eliminar.append(continentes.iloc[index,0])
+    
+for index, _ in GSAF.iterrows():
+    pais=GSAF.loc[index,"Country"]
+    if pais in paises_a_eliminar:
+        GSAF=GSAF.drop(index, axis=0)
+    
+    
+
+
+
 #%% hacemos la modificacion
 for index, _, _ in filas_inconsistentes:
     GSAF.loc[index,"Type"]="QUESTIONABLE"
     GSAF.loc[index,"ColorFondo"]=16764057
 
 #%% Con un error acumulado de menos del 2% aprox 140 casos hacemos los graficos
-cantidad_incidentes_por_tipo=sql^ """SELECT Type, COUNT(Country) AS Cantidad FROM GSAF GROUP BY TYPE ORDER BY Cantidad ASC  """
+cantidad_incidentes_por_tipo=sql^ """SELECT Type, COUNT(Country) AS Cantidad FROM GSAF GROUP BY TYPE ORDER BY Cantidad ASC"""
 
-cantidad_incidentes_por_tipo_por_pais=sql^ """SELECT Country, Type, COUNT(Country) AS Cantidad FROM GSAF GROUP BY  Country, Type HAVING Cantidad>2 ORDER BY Cantidad DESC  """
+cantidad_incidentes_por_tipo_por_pais=sql^ """SELECT Country, Type, COUNT(Country) AS Cantidad FROM GSAF GROUP BY  Country, Type HAVING Cantidad>0 ORDER BY Cantidad DESC  """
 
-#%% Hacemos unos graficos de los resultados
+#%% Hacemos un grafico de el resultado para nuestro primer objetivo
 fig, ax = plt.subplots()
 barras=ax.barh([1,2,3,4,5],cantidad_incidentes_por_tipo["Cantidad"],color=["gold","lawngreen","blue","orange","tan"])
 etiquetas_y=list(cantidad_incidentes_por_tipo["Type"])
 ax.bar_label(barras, padding=0, label_type='edge',rotation=270)
 ax.set_yticks([1,2,3,4,5])
 ax.set_yticklabels(etiquetas_y)
+ax.set_ylabel("Tipo de ataque")
+ax.set_xlabel("Cantidad de ataques")
 plt.tight_layout()
-#%%
-fig, ax = plt.subplots()
+#%%procesamiento para graficar
+#le asignamos un id a cada pais ara ubicarlo en el grafico
+paises_distintos=sql^"""SELECT DISTINCT Country FROM cantidad_incidentes_por_tipo_por_pais ORDER BY Cantidad asc"""
 
+id_paises={}
+paises_id={}
+for id in range(1,len(paises_distintos)+1):
+    pais=paises_distintos.iloc[id-1,0]
+    id_paises[id]=pais
+    paises_id[pais]=id
 
+dict_provoked={}
+dict_unprovoked={}
+dict_att_on_boat={}
+dict_sea_disaster={}
+dict_Questionable={}
+
+Provoked_paises=sql^"""SELECT DISTINCT Country, Cantidad FROM cantidad_incidentes_por_tipo_por_pais WHERE Type='PROVOKED'""" 
+Unprovoked_paises=sql^"""SELECT DISTINCT Country, Cantidad  FROM cantidad_incidentes_por_tipo_por_pais WHERE Type='UNPROVOKED'"""
+Sea_disaster_paises=sql^"""SELECT DISTINCT Country, Cantidad  FROM cantidad_incidentes_por_tipo_por_pais WHERE Type='SEA DISASTER'"""
+Att_on_boat_paises=sql^"""SELECT DISTINCT Country, Cantidad  FROM cantidad_incidentes_por_tipo_por_pais WHERE Type='ATTACKS ON BOAT'"""
+Questianble_paises=sql^"""SELECT DISTINCT Country, Cantidad  FROM cantidad_incidentes_por_tipo_por_pais WHERE Type='QUESTIONABLE'"""
+
+for i in range(len(Provoked_paises)):
+    fila=Provoked_paises.iloc[i]
+    pais=fila[0]
+    valor=fila[1]
+    dict_provoked[paises_id[pais]]=valor
+
+for i in range(len(Unprovoked_paises)):
+    fila=Unprovoked_paises.iloc[i]
+    pais=fila[0]
+    valor=fila[1]
+    dict_unprovoked[paises_id[pais]]=valor
+
+for i in range(len(Att_on_boat_paises)):
+    fila=Att_on_boat_paises.iloc[i]
+    pais=fila[0]
+    valor=fila[1]
+    dict_att_on_boat[paises_id[pais]]=valor
+    
+for i in range(len(Sea_disaster_paises)):
+    fila=Sea_disaster_paises.iloc[i]
+    pais=fila[0]
+    valor=fila[1]
+    dict_sea_disaster[paises_id[pais]]=valor
+    
+for i in range(len(Questianble_paises)):
+    fila=Questianble_paises.iloc[i]
+    pais=fila[0]
+    valor=fila[1]
+    dict_Questionable[paises_id[pais]]=valor
+
+fig , ax=plt.subplots()
